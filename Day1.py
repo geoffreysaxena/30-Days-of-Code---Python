39aaf0069fce2cb11907d6385b76dc612ecebce4
Pratik Geoffrey Saxena <54907139+GeoffreySaxena@users.noreply.github.com>
2021-01-02 10:20:04 +0530

    Create Day1.py

---
+++ Day1.py
@@ -0,0 +1,16 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+x = 0
+y = 0
+# Read and save an integer, double, and String to your variables.
+x = int(input())
+y = float(input())
+z = input()
+# Print the sum of both integer variables on a new line.
+print(x + i)
+# Print the sum of the double variables on a new line.
+print(y + d)
+# Concatenate and print the String variables on a new line
+print(s + z)
