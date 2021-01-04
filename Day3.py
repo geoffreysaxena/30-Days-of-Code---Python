54332886b11546970b29c566a62658e5ebeb465c
Pratik Geoffrey Saxena <54907139+GeoffreySaxena@users.noreply.github.com>
2021-01-04 21:36:01 +0530

    Create Day3.py

---
+++ Day3.py
@@ -0,0 +1,22 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    N = int(input())
+
+if(N % 2 == 0):
+    if(N >= 2 and N < 6):
+        print('Not Weird')
+    elif(N >= 6 and N <= 20):
+        print('Weird')
+    else:
+        print('Not Weird')
+else:
+    print('Weird')    
