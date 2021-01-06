d213669cbd63a5cbf990f880635a294b6103f792
Pratik Geoffrey Saxena <54907139+GeoffreySaxena@users.noreply.github.com>
2021-01-06 22:31:22 +0530

    Create Day5.py

---
+++ Day5.py
@@ -0,0 +1,15 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input())
+    
+for x in range(1, 11):
+    print(str(n) + " x " + str(x) + " = " + str(n * x))
